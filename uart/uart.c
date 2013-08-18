dff3a2b1829d29f1f0439ff1c41f34a326fe2a1f
Michal Belica <devel@beli.sk>
2013-08-18 16:46:21 +0200

    parametrize UART register names

--- uart/uart.c
+++ uart/uart.c
@@ -24,22 +24,53 @@
 
 #define CONVERT_CRLF 1
 
+#if defined (__AVR_ATmega328P__) || defined (__AVR_ATmega328__)
+#	define _UBRRH UBRR0H
+#	define _UBRRL UBRR0L
+#	define _UCSRA UCSR0A
+#	define _UCSRB UCSR0B
+#	define _UCSRC UCSR0C
+#	define _U2X U2X0
+#	define _UDRE UDRE0
+#	define _UDR UDR0
+#	define _UCSZ0 UCSZ00
+#	define _UCSZ1 UCSZ01
+#	define _RXEN RXEN0
+#	define _TXEN TXEN0
+#	define _RXC RXC0
+#else
+#	warning "Unknown part, guessing register names."
+#	define _UBRRH UBRRH
+#	define _UBRRL UBRRL
+#	define _UCSRA UCSRA
+#	define _UCSRB UCSRB
+#	define _UCSRC UCSRC
+#	define _U2X U2X
+#	define _UDRE UDRE
+#	define _UDR UDR
+#	define _UCSZ0 UCSZ0
+#	define _UCSZ1 UCSZ1
+#	define _RXEN RXEN
+#	define _TXEN TXEN
+#	define _RXC RXC
+#endif
+
 int uart_getchar(FILE *stream);
 int uart_putchar(char c, FILE *stream);
 
 FILE uart_io = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
 
 void uart_init(void) {
-	UBRR0H = UBRRH_VALUE;
-	UBRR0L = UBRRL_VALUE;
+	_UBRRH = UBRRH_VALUE;
+	_UBRRL = UBRRL_VALUE;
 
 #if USE_2X
-	UCSR0A |= _BV(U2X0);
+	_UCSRA |= _BV(_U2X);
 #else
-	UCSR0A &= ~_BV(U2X0);
+	_UCSRA &= ~_BV(_U2X);
 #endif
-	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
-	UCSR0B = _BV(RXEN0) | _BV(TXEN0);
+	_UCSRC = _BV(_UCSZ1) | _BV(_UCSZ0);
+	_UCSRB = _BV(_RXEN) | _BV(_TXEN);
 }
 
 int uart_putchar(char c, FILE *stream) {
@@ -48,15 +79,15 @@ int uart_putchar(char c, FILE *stream) {
 		uart_putchar('\r', stream);
 	}
 #endif
-	loop_until_bit_is_set(UCSR0A, UDRE0);
-	UDR0 = c;
+	loop_until_bit_is_set(_UCSRA, _UDRE);
+	_UDR = c;
 	return (int)c;
 }
 
 int uart_getchar(FILE *stream) {
 	unsigned char c;
-	loop_until_bit_is_set(UCSR0A, RXC0);
-	c = UDR0;
+	loop_until_bit_is_set(_UCSRA, _RXC);
+	c = _UDR;
 #ifdef CONVERT_CRLF
 	if( c == '\r' ) {
 		c = '\n';
